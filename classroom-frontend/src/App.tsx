9199e09c6aeb69a753a634825f19452dd479db29
Daniyal <thedaniiyal@gmail.com>
2026-08-17 17:35:45 +0530

    initial commit

---
+++ classroom-frontend/src/App.tsx
@@ -0,0 +1,75 @@
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+} from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { BrowserRouter, Route, Routes, Outlet } from "react-router";
+import routerProvider, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router";
+import { dataProvider } from "./providers/data";
+import { ErrorComponent } from "./components/refine-ui/layout/error-component";
+import { Layout } from "./components/refine-ui/layout/layout";
+import { Header } from "./components/refine-ui/layout/header";
+import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { Toaster } from "./components/refine-ui/notification/toaster";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import Dashboard from "@/pages/dashboard";
+import { Home } from "lucide-react";
+import "./App.css";
+
+/* 2: 10 */
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <ThemeProvider>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              notificationProvider={useNotificationProvider()}
+              routerProvider={routerProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "GBUQlc-Ty6a2R-uCaXVJ",
+              }}
+              resources={[
+              {
+                name: 'dashboard',
+                list: '/',
+                meta: { label: 'Home', icon: <Home />}
+              }
+            ]}
+            >
+              <Routes>
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }>
+                  <Route path="/" element={<Dashboard />} />
+                </Route>
+              </Routes>
+              <Toaster />
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ThemeProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
